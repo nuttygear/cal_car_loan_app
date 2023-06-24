a4db3077f65b72049dd8849ed7c4704f7a703759
nuttygear <chanintorn.comsci@gmail.com>
2023-06-24 19:36:46 +0700

    1st commit....

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:cal_car_loan_app/views/introduction_ui.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    //เรียก widget หลักของแอปฯ
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: IntroductionUI(),
+    ),
+  );
+}
